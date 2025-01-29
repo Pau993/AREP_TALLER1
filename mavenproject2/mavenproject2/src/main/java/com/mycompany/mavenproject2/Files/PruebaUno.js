db474faf245662f621a0d6ecad34ac324a37d5b9
nataliapaez993@gmail.com <nataliapaez993@gmail.com>
2025-01-28 20:12:00 -0500

    Organizar archivos

--- mavenproject2/mavenproject2/src/main/java/com/mycompany/mavenproject2/Files/PruebaUno.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("actionButton").addEventListener("click", function() {
-    const messageElement = document.getElementById("message");
-    messageElement.textContent = "Button clicked!";
-    messageElement.style.color = "#6200ea";
-});
